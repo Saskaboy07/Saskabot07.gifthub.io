c070a072138ff38a4b02786b026b1c5e281bb428
Saskaboy07 <jordancartwright2007@gmail.com>
2024-09-24 15:19:47 -0600

    to today i added different background sand kyle broke it lmaoo

--- interactive scene/sketch.js
+++ interactive scene/sketch.js
@@ -5,74 +5,108 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let currentBack = 0;
+let currentColor = "cyan";
+let movenment = 200;
 
 function setup() {
-  createCanvas( windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
-  background("cyan");
-  
-  
 
-
-  
-
-   night();
-   sun();
-   clouds();
-   sand();
-   name();
-   moon();
+  background(currentColor);
+  turnNight();
+  sun();
+  clouds();
+  sand();
+  artistMark();
+  moon();
+  move();
+  fill("white");
+  ellipse(movenment, 600, 70, 20);
 }
 
 function sun(){
+  //thus is for my
   fill("yellow");
   circle(50,40,60)
-  
-
-
-
 }
 
 function clouds(){
+  //this is for my clouds
   fill("white");
   circle(50,50,50);
   circle(70,60,40);
   circle(90,50,50);
   circle(70,40,40);
-  noStroke();
-
+  
 }
   
-function name(){
+function artistMark(){
+  //this is my name
   fill("Black");
   textSize(20);
-  text("Jordan", 680, 380);
-
-
+  text("Jordan", 860, 460);
 }
 
 function sand(){
+  //this is for the ground
   fill("Tan");
-  square(0, 250, 800, 0);
-  noStroke();
-
-
-
+  rect(0, 300, width, 800);
 }
 
 function moon(){
+  //this is for the moon
   fill("gray");
   circle(695, 40 ,50);
   fill("cyan");
-  circle(675, 40, 50);
-
+  
 }
-function night(){
-  if(keyIsPressed === "w"){
- background("");
+
+
+
+function turnNight(){
+   //change background to black
+  if(currentBack === 0){
+    currentColor = "cyan";
+  }
+    
+  if(currentBack === 1){
+    currentColor = "black";
+  }
+  
+  if(currentBack === 2){
+    currentColor = "red";
+  }
+  if(currentBack === 3){
+    currentColor = "pink";
   }
 
+}
+  
+function mouseClicked(){
+  if(mouseButton === LEFT && currentBack < 3){
+    currentBack += 1;
+  
+  }
+  else currentBack = 0;
 
+}
+
+function move(){
+  if(keyPressed === true){
+
+    if(keyCode === LEFT_ARROW){
+      movenment = movenment - 1;
+    }
+
+    else if(keyCode === RIGHT_ARROW){
+      movenment = movenment + 1;
+    }
+
+  }
+  
+  
 }

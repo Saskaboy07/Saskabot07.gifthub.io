dced220112fd4cec89882370894cac5577310c00
Saskaboy07 <jordancartwright2007@gmail.com>
2024-10-24 12:53:47 -0600

    ujfuegfuefguwrqfgr3uio

---
+++ cars! cars! cars!/sketch.js
@@ -0,0 +1,18 @@
+// cars! cars! cars!
+// jordan cartwright
+// oct 22nd 2024
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+}
+
+
+function drawroad(){
+  
+
+
+}

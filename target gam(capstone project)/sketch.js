1a1c864ab19807fcc026452b006f3e3acd2baaac
Saskaboy07 <jordancartwright2007@gmail.com>
2024-12-04 15:22:17 -0600

    started target game

--- target gam(capstone project)/sketch.js
+++ target gam(capstone project)/sketch.js
@@ -1,9 +1,38 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// target game
+// jordan
+// 12/4/2024
+
+//images to load
+let backImage, backImageReport, barrelImage;
+let baseImage, cannonballImage, targetImage;
+let explosionImages = [];
+let shotsRemainImages = [];
+let targetsHitImages = [];
+
+
+function preload(){
+  backImage = loadImage("assets/background");
+  backImageReport = loadImage("assets/backgroundReport.png");
+  barrelImage = loadImage("assets/barrel.png");
+  cannonballImage = loadImage("assets/cannonball.png");
+  targetImage = loadImage("assets/target.png");
+
+  for(let i = 0; i < 6; i++){
+    explosionImages[i] = loadImage("assets/ex"+i+"png");
+
+  }
+
+  for(let i = 0; i < 21; i++){
+    shotsRemainImages[i] = loadImage("assets/shots"+i+"png");
+
+  }
+
+  for(let i = 0; i < 21; i++){
+    shotsRemainImages[i] = loadImage("assets/shots"+i+"png");
+
+  }
+}
+
 
 
 function setup() {
@@ -11,5 +40,5 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  image(backImage,0,0);
 }

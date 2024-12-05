45636c158d225821d420c8b21b4b06728892958d
Saskaboy07 <jordancartwright2007@gmail.com>
2024-12-05 13:02:05 -0600

    finished video one, background is working, on video 2 now

---
+++ target gam(capstone project)/game.js
@@ -0,0 +1,32 @@
+class game{
+    constructor(){
+        this.connanangle = 45;
+        this.connanpower = 10;
+        this.shots = [];
+  }
+
+  play(){
+    //called once per frame (acts like draw)
+    imageMode(CENTER);
+    image(backImage,0,0);
+
+    //process and fraw every cannonball
+
+    //process and draw every stroke particle
+
+    //draw the cannon
+    this.displayCannon();
+
+  }
+
+  displayCannon(){
+    imageMode(CENTER);
+    push();
+    translate(73, 535);
+    rotate(radians(360 - this.connanangle));
+    image(barrelImage,0,0);
+    image(baseImage,0,0);
+    pop();
+
+  }
+}
--- target gam(capstone project)/sketch.js
+++ target gam(capstone project)/sketch.js
@@ -10,29 +10,6 @@ let shotsRemainImages = [];
 let targetsHitImages = [];
 
 
-function preload(){
-  backImage = loadImage("assets/background");
-  backImageReport = loadImage("assets/backgroundReport.png");
-  barrelImage = loadImage("assets/barrel.png");
-  cannonballImage = loadImage("assets/cannonball.png");
-  targetImage = loadImage("assets/target.png");
-
-  for(let i = 0; i < 6; i++){
-    explosionImages[i] = loadImage("assets/ex"+i+"png");
-
-  }
-
-  for(let i = 0; i < 21; i++){
-    shotsRemainImages[i] = loadImage("assets/shots"+i+"png");
-
-  }
-
-  for(let i = 0; i < 21; i++){
-    shotsRemainImages[i] = loadImage("assets/shots"+i+"png");
-
-  }
-}
-
 
 
 function setup() {
